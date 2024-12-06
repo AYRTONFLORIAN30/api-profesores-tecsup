e497ff72f9e2903f29322e708877897c4328f5d3
AYRTONFLORIAN30 <ayrton.florian@tecsup.edu.pe>
2024-12-06 14:18:41 -0500

    Subir proyecto API

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000; // Valor predeterminado para el puerto
 
 // Middleware para permitir el envío de datos en formato JSON
 app.use(express.json());
